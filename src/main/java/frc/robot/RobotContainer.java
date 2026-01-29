337121959c89a0d46d8e308a6f473301b7392f27
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2026-01-29 15:10:23 +0800

    refactor: remove unused SysId routines and related code from CommandSwerveDrivetrain

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,14 +10,11 @@ import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.subsystems.Intake;
 
 import frc.robot.generated.TunerConstants;
@@ -74,13 +71,6 @@ public class RobotContainer {
         driverCtrl.b().whileTrue(drivetrain.applyRequest(() ->
             point.withModuleDirection(new Rotation2d(-driverCtrl.getLeftY(), -driverCtrl.getLeftX()))
         ));
-
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-        driverCtrl.back().and(driverCtrl.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        driverCtrl.back().and(driverCtrl.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        driverCtrl.start().and(driverCtrl.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        driverCtrl.start().and(driverCtrl.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // Reset the field-centric heading on left bumper press.
         driverCtrl.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
